0b611415b028a6cc848e01762d1a2f2b21956193
speedssster <lastprogamer0@gmail.com>
2022-02-22 01:58:19 +0330

    using new responses incontact require action.

--- telegram/actions/contact_required.py
+++ telegram/actions/contact_required.py
@@ -2,6 +2,8 @@ from django.core.cache import cache
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+from telegram.responses import request_phone_number, invalid_phone_number, request_username
+
 
 User = get_user_model()
 
@@ -15,37 +17,14 @@ def contact_required(message: dict):
         contact_user_id = message["contact"]["user_id"]
         contact_phone_number = message["contact"]["phone_number"]
     else:
-        return {
-            "method": "sendMessage",
-            "chat_id": tg_id,
-            "text": _("Please share your phone number to Signup:"),
-            "reply_markup": {
-                "keyboard": [[{"text": _("Share Contact"), "request_contact": True}]],
-                "resize_keyboard": True,
-                "one_time_keyboard": True,
-            },
-        }
+        return request_phone_number(tg_id)
 
     # ACTION:
     user = User.objects.get(telegram_id=tg_id)
     if tg_id != contact_user_id:
-        return {
-            "method": "sendMessage",
-            "chat_id": tg_id,
-            "text": _("The shared contact is not yours!"),
-            "reply_markup": {
-                "keyboard": [[{"text": _("Share Contact"), "request_contact": True}]],
-                "resize_keyboard": True,
-                "one_time_keyboard": True,
-            },
-        }
+        return invalid_phone_number(tg_id)
     else:
         user.phone_number = contact_phone_number.lstrip("+")
         user.save()
         cache.set(tg_id, "username_required")
-        return {
-            "method": "sendMessage",
-            "chat_id": tg_id,
-            "text": _("Please enter your desired Username:"),
-            "reply_markup": {"remove_keyboard": True},
-        }
+        return request_username(tg_id)
