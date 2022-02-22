bbeef1e282f8dff96248cc1d30c15a913b54f58e
speedssster <lastprogamer0@gmail.com>
2022-02-22 20:21:48 +0330

    welcome payload added.

--- telegram/payloads/__init__.py
+++ telegram/payloads/__init__.py
@@ -3,3 +3,4 @@ from .invalid_phone_number import invalid_phone_number
 from .request_username import request_username
 from .username_duplicate_error import username_duplicate_error
 from .username_validation_error import username_validation_error
+from .welcome import welcome
---
+++ telegram/payloads/welcome.py
@@ -0,0 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+
+
+def welcome(tg_id, username):
+    return {
+        "method": "sendMessage",
+        "chat_id": tg_id,
+        "text": _(
+            f"Hi {username} 👋 \nWelcome to DjangoQa official bot. If you need any assistance, I’m always here."
+        ),
+        "reply_markup": {
+            "inline_keyboard": [
+                [{"text": _("Login"), "callback_data": "login"}]
+            ],
+        },
+    }
