1cac5dd49f73717afb9e8921a251f29e28cc9580
MrMohammadY <m.yazdani000@gmail.com>
2022-02-24 23:54:56 +0330

    fix(accounts/views): Fix otp api view

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -2,24 +2,19 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
-from accounts.api.serializers import OTPEmailSerializer
-from accounts.tasks import email_verification
+from accounts.api.serializers import OTPPhoneNumberSerializer
+from accounts.tasks import phone_number_verification
 
 
 class OTPAPIView(GenericAPIView):
     """
     Create a one time password and send for user email or phone number.
     """
-    serializer_class = OTPEmailSerializer
-
-    # TODO-1: add lookup_url_kwarg for email and phone number opt send
-    # TODO-2: edit url
-    # TODO-3: override get_serializer method and add phone number serializer
-    # TODO-4: override get_otp_sender method and add phone_number_verification
+    serializer_class = OTPPhoneNumberSerializer
 
     @staticmethod
     def get_otp_sender():
-        return email_verification
+        return phone_number_verification
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
