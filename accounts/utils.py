a93e1fc9da47b45b22af6579dfcf2c83b6cfdb27
MrMohammadY <m.yazdani000@gmail.com>
2022-02-24 23:50:02 +0330

    fix(accounts): Fix redis class and use default django redis

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,14 +1,12 @@
 import random
 
-import redis
-
 from datetime import datetime
 
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
+from django.core.cache import cache
 from django.contrib.auth.hashers import make_password, check_password
 
 from config import settings
+from config.settings import OTP_EXPIRE_TIME
 
 
 def user_directory_path(instance, filename):
@@ -23,38 +21,23 @@ class RedisConnection:
     """
     _instance = None
 
-    def __init__(self):
-        self.host = settings.REDIS_HOST
-        self.port = settings.REDIS_PORT
-        self.db = settings.REDIS_DB
-
     @classmethod
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(*args, **kwargs)
         return cls._instance
 
-    def __redis_connect(self):
-        return redis.Redis(host=self.host, port=self.port, db=self.db)
-
     @staticmethod
-    def __redis_disconnect(redis_instance):
-        redis_instance.close()
-
-    def set_otp(self, key):
-        redis_instance = self.__redis_connect()
+    def set_otp(key):
         otp_code = random.randint(1111, 9999)
-        redis_instance.set(key, make_password(str(otp_code)))
-        redis_instance.expire(key, 180)
-        self.__redis_disconnect(redis_instance)
+        cache.set(key, make_password(str(otp_code)), OTP_EXPIRE_TIME)
         return otp_code
 
-    def verify_otp(self, key, otp_verify):
-        redis_instance = self.__redis_connect()
-        otp_code = redis_instance.get(key)
-        self.__redis_disconnect(redis_instance)
+    @staticmethod
+    def verify_otp(key, otp_verify):
+        otp_code = cache.get(key)
         if otp_code:
-            if check_password(otp_verify, otp_code.decode('utf8')):
+            if check_password(otp_verify, otp_code):
                 return True, True
             return True, False
         return False, False
